92ef91f3dab40d08639f54ea1d894f173c7124f6
Gabriel Sevilha <58699454+gabrielsevilha@users.noreply.github.com>
2021-06-06 19:50:31 -0300

    Update text_engine.h

--- text_engine.h
+++ text_engine.h
@@ -1,17 +1,18 @@
-/*Write By: Gabriel Sevilha <3*/
+/* by Gabriel Sevilha <3
 
-/*Dependences: OpenGL 1.1 and freetype2*/
+This is a unique header library, that serves that a fast way to draw text using OpenGL and freetype2, and serves also as example of freetype2 library.
+The code are write using OpenGL Imediate Mode because beside a library, this code need be understandeble to learn freetype2, and also be used in low-end computers.
 
-/*  Obs 1 : You need orthographic projection matrix, you get this with:
-		glOrtho(0,view_port_width,view_port_height,0,0,1);
-	Obs 2 : You need load OpenGL functions before include this library, as the exemple bellow.
-	Obs 2.1 : In the exemple opengl was loaded with glfw3 library.
-*/
+Dependences: OpenGL 1.1 and freetype2
 
-/*Exemple with glfw3:
+This library use OpenGL Imediate Mode.
+You need orthographic projection matrix, you get this with:
+glOrtho(0,view_port_width,view_port_height,0,0,1);
+You need load OpenGL functions before include this library, as the exemple bellow.
+In the exemple opengl was loaded with glfw3 library.
+Exemple with glfw3:
 
 #include<GLFW/glfw3.h> 
-
 #include"text_engine.h"
 
 int main(){
@@ -19,7 +20,6 @@ int main(){
 	glfwInit();
 	GLFWwindow* window = glfwCreateWindow(800,600,"text_engine",0,0);
 	glfwMakeContextCurrent(window);
-
 	glOrtho(0,800,600,0,0,1);
 	
 	Font* font = createFont("DejaVuSerif.ttf",48);
@@ -29,9 +29,8 @@ int main(){
 		glfwWaitEvents();
 		
 		glColor4f(1,1,1,1);
-
 		drawText(font, "Hello, World!", 800/2-getSizeText(font,"Hello, World!")/2, 600/2-font->size);
-
+    
 		glfwSwapBuffers(window);
 		
 	}
@@ -41,12 +40,13 @@ int main(){
 	return 0;
 	
 }
+
+Exemple was compiled with: -lglfw -lGL `pkg-config --cflags --libs freetype2`
+
+Obs: Of course, glfw3 is opitional for this library, but only make sure that OpenGL 1.1 funcions was loaded.
+
 */
-
-/*Compile exemple with: -lglfw3 -lGL `pkg-config --cflags --libs freetype2`*/
-
-/*Of course, glfw3 is opitional for this library, but only make sure that OpenGL 1.1 funcions was loaded.*/
-
+	
 #ifndef TEXT_ENGINE
 #define TEXT_ENGINE
 
